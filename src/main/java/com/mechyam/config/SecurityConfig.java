4160b540574659966f169b853228327331c15349
Praveen Kumar <kumarpraveen7604@gmail.com>
2025-11-24 10:50:04 +0530

    give CORS permisson

--- src/main/java/com/mechyam/config/SecurityConfig.java
+++ src/main/java/com/mechyam/config/SecurityConfig.java
@@ -57,7 +57,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+//        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://mechyam-frontend-gold.vercel.app"
+        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
